75bba47efc33580e575857462b75c45a4bdefab2
AvendanoisPepe <jsavendano62@misena.edu.co>
2024-03-05 16:59:18 -0500

    intento de apls

--- src/components/Aplicativo/Aplicativo.jsx
+++ src/components/Aplicativo/Aplicativo.jsx
@@ -1,3 +1,34 @@
-export default function Aplicativos() {
-	return <div>maria la del barrio soy</div>
+import { useState } from 'react'
+import './apStyle.scss'
+import datosApli from './dataAplicativos'
+
+const Aplicativos = () => {
+	// selectedOption para almacenar la opción seleccionada y setSelectedOption para actualizar el estado de la opción seleccionada.
+	const [selectedOption, setSelectedOption] = useState('jet')
+
+	return (
+		<section className="aplicativos">
+			<h1 className="aplicativos__title">APLICATIVOS WEB</h1>
+			<div>
+				{/* le damos el valor por medio del onclick */}
+				<button onClick={() => setSelectedOption('jet')}>JetSmart</button>
+				<button onClick={() => setSelectedOption('mar')}>
+					Aplicativos Atento
+				</button>
+				{/* si hay equivalencia se renderiza el contenido de onlyFans */}
+				{selectedOption === 'jet' && (
+					<div>
+						<div>maria la del abrrio soy</div>
+					</div>
+				)}
+				{selectedOption === 'mar' && (
+					<div>
+						<div>pedro es gat</div>
+					</div>
+				)}
+			</div>
+		</section>
+	)
 }
+
+export default Aplicativos
---
+++ src/components/Aplicativo/dataAplicativos.js
@@ -0,0 +1,75 @@
+const datosApli = [
+	{
+		modulo: 'jet',
+		nombre: 'DINSIDE',
+		enlace: 'Ingreso al sistema [url=http://jsprod.dinside2.cl/menu]',
+		imagen: 'img1',
+	},
+	{
+		modulo: 'jet',
+		nombre: 'FACTUTRACIÓN ELECTRONICA',
+		enlace: 'http://190.153.213.27/opendte/login.php',
+		imagen: 'img2',
+	},
+	{
+		modulo: 'jet',
+		nombre: 'TIPIFICADOR',
+		enlace:
+			'http://colbogweb14/Centro_de_Ayuda_Jetsmart/Login.aspx?__eo_obj_states=ASEBGBdjdGwwMF9TbGlkZU1lbnUxOjBELDFCLA==',
+		imagen: 'img3',
+	},
+	{
+		modulo: 'jet',
+		nombre: 'PAGINA WEB JETSMART',
+		enlace:
+			'Vuelos Baratos - Pasajes en Avión | JetSMART Chile Sitio Oficial [url=https://www.jetsmart.com/cl/es/]',
+		imagen: 'img4',
+	},
+	{
+		modulo: 'jet',
+		nombre: 'FIVE9',
+		enlace: 'Login | Five9 [url=https://login.five9.com/]',
+		imagen: 'img5',
+	},
+	{
+		modulo: 'jet',
+		nombre: '15BELOW',
+		enlace:
+			'15below [url=https://prd.15below.travel/account/sign-in?ReturnUrl=%2F]',
+		imagen: 'img6',
+	},
+	{
+		modulo: 'jet',
+		nombre: 'KEEPCON',
+		enlace:
+			'Keepcon [url=https://cem.keepcon.com/sessions/new#filter/1073741824]',
+		imagen: 'img7',
+	},
+	{
+		modulo: 'atento',
+		nombre: 'GAP',
+		enlace: 'http://172.16.183.76/ControlAsistencia/login.php',
+		imagen: 'img8',
+	},
+	{
+		modulo: 'atento',
+		nombre: 'EN DIRECTO',
+		enlace: 'http://172.16.183.76/endirecto/web/index.php/main/login',
+		imagen: 'img9',
+	},
+	{
+		modulo: 'atento',
+		nombre: 'CAJERO KAKTUS',
+		enlace: 'https://cajerokactus.atento.com.co:8443/WebKactus/frmLogin.aspx',
+		imagen: 'img10',
+	},
+	{
+		modulo: 'atento',
+		nombre: 'ECCUS',
+		enlace:
+			'http://172.16.183.59/tm_eccus/procesos/consulta_toma_evaluacion.jsp',
+		imagen: 'img11',
+	},
+]
+
+export default datosApli;
