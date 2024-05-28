54794ca052d1eda3f8b7a5fa7bd379c113082212
nedagosa-cod <nestorcode4004@gmail.com>
2024-05-28 14:51:55 -0500

    update localbase and excelfile to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import './styles.scss'
 import projectNavbar from './dataNavbar'
-import { createContext, useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import DropDown from './DropDown'
 import LinkRoute from './LinkRoute'
 import imgLogo from '../../assets/images/index/logoMain.png'
@@ -9,13 +9,15 @@ import IconUpload from '../../icons/IconUpload'
 import IconTheme from '../../icons/IconTheme'
 import InconSpell from '../../icons/InconSpell'
 import GlobalContext from '../../context/GlobalContext'
+import { createPortal } from 'react-dom'
 
 export default function Navbar() {
 	const [openNav, setOpenNav] = useState(false)
 	const [scrollPx, setScrollPx] = useState(0)
 	const [dataSearch, setDataSearch] = useState('')
+	const [windowDB, setWindowDB] = useState(false)
 	const scrollContainerRef = useRef(null)
-	const { WTLocalbase } = createContext(GlobalContext)
+	const { readExcelFile } = useContext(GlobalContext)
 
 	const openCloseNavBar = () => {
 		if (openNav) return setOpenNav(!openNav)
@@ -39,20 +41,30 @@ export default function Navbar() {
 			card.classList.remove('hide')
 		})
 	}
-	const handleSubmit = e => {
-		e.preventDefault()
-		console.log(WTLocalbase)
+	const setsClick = e => {
+		switch (e.target.name) {
+			case 'theme':
+				console.log('theme')
+				break
+			case 'upload':
+				setWindowDB(true)
+				break
+			default:
+		}
 	}
+
 	useEffect(() => {
 		const handleScroll = () => {
-			console.log('Píxeles desplazados verticalmente:', scrollContainerRef.current.scrollTop)
 			setScrollPx(scrollContainerRef.current.scrollTop)
 		}
-
 		if (scrollContainerRef.current) {
 			scrollContainerRef.current.addEventListener('scroll', handleScroll)
 		}
-
+		document.body.addEventListener('keydown', e => {
+			if (e.key == 'Escape') {
+				setWindowDB(false)
+			}
+		})
 		return () => {
 			if (scrollContainerRef.current) {
 				scrollContainerRef.current.removeEventListener('scroll', handleScroll)
@@ -97,20 +109,47 @@ export default function Navbar() {
 			</ul>
 
 			{/* {version} */}
-			<form className="sidebar__sets" onSubmit={handleSubmit}>
-				<button className="sidebar__sets--btn">
+			<div className="sidebar__sets">
+				<button className="sidebar__sets--btn" name="upload" onClick={setsClick}>
 					<IconUpload />
 				</button>
-				<button className="sidebar__sets--btn">
+				<button className="sidebar__sets--btn" name="spellcheck">
 					<InconSpell />
 				</button>
-				<button className="sidebar__sets--btn">
+				<button className="sidebar__sets--btn" name="theme">
 					<IconTheme />
 				</button>
-			</form>
+			</div>
 			<div className="sidebar__versionbx" id="sideVersion">
 				v1.0.0
 			</div>
+			{windowDB &&
+				createPortal(
+					<section className="templates-xls">
+						<div className="templates-xls--left">
+							<form className="file-upload-form">
+								<label htmlFor="file" className="file-upload-label">
+									<div className="file-upload-design">
+										<svg viewBox="0 0 640 512" height="1em">
+											<path d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"></path>
+										</svg>
+										<p>Arrastra y Suelta la base</p>
+										<p>o</p>
+										<span className="browse-button">Carga la base</span>
+									</div>
+									<input type="file" id="file" onChange={readExcelFile} multiple />
+								</label>
+							</form>
+						</div>
+						<div className="templates-xls--right">
+							<span>Plantillas</span>
+							<article className="templates">
+								No hay plantilas disponibles para la web training
+							</article>
+						</div>
+					</section>,
+					document.getElementById('portal')
+				)}
 		</header>
 	)
 }
--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -1,11 +1,64 @@
-import { createContext, useState } from 'react'
+import { createContext } from 'react'
 import Localbase from 'localbase'
+import Swal from 'sweetalert2'
+import readXlsxFile from 'read-excel-file'
 
 const GlobalContext = createContext()
+const WTLocalbase = new Localbase('db_nombre_campana')
 
 const GlobalProvider = ({ children }) => {
-	let WTLocalbase = new Localbase('db_nombre_campana')
-	const data = { WTLocalbase }
+	const templatesDDBB = ['arbol']
+	const readExcelFile = async e => {
+		const maps = {
+			arbol: {
+				CODIGO: 'CODIGO',
+				DATOS: 'DATOS',
+				ESCALAMIENTO: 'ESCALAMIENTO',
+				AREA: 'AREA',
+			},
+		}
+		// recorre los archivos cargados y valida si son bases correctas de la web training
+		const filesList = e.target.files
+		for (let i = 0; i < filesList.length; i++) {
+			const file = filesList[i]
+			const fileName = file.name.split('.')[0]
+			if (templatesDDBB.includes(fileName)) {
+				readXlsxFile(file, { map: maps[fileName] }, { trim: true })
+					.then(({ rows }) => {
+						rows.forEach((db, id) => {
+							WTLocalbase.collection(fileName).delete()
+							WTLocalbase.collection(fileName).add({
+								id,
+								...db,
+							})
+						})
+					})
+					.then(() => {
+						Swal.fire({
+							icon: 'success',
+							title: 'Base de datos actualizada',
+							text: 'Por favor presiona F5 o actualiza la pagina para cargar la base.',
+						})
+					})
+					.catch(error => {
+						Swal.fire({
+							icon: 'error',
+							title: 'Estas cargando una base de datos incorrecta',
+							text: 'Verifica que los datos en el excel esten correctamente insertados y las columnas tengan los nombres correspondientes.',
+							footer: 'Error: ' + error,
+						})
+					})
+			} else {
+				Swal.fire({
+					icon: 'error',
+					title: 'Estas cargando una base de datos incorrecta',
+					text: 'La base de datos que estas cargando no es la correcta, verifica el nombre del archivo excel que estas cargando. valida si tiene el nombre corrector de la base o si las columnas estan completas.',
+				})
+			}
+		}
+	}
+
+	const data = { WTLocalbase, readExcelFile }
 	return <GlobalContext.Provider value={data}>{children}</GlobalContext.Provider>
 }
 
