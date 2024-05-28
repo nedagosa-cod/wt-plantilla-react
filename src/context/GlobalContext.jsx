208b822cff6402eb4af27ffc2b8680332695be2f
nedagosa-cod <nestorcode4004@gmail.com>
2024-05-28 16:01:14 -0500

    problem idb

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -17,37 +17,48 @@ const GlobalProvider = ({ children }) => {
 				AREA: 'AREA',
 			},
 		}
-		// recorre los archivos cargados y valida si son bases correctas de la web training
-		const filesList = e.target.files
+
+		const map = {
+			'START DATE': 'CODIGO',
+			'NUMBER OF STUDENTS': 'DATOS',
+			COURSE: {
+				course: {
+					'IS FREE': 'ESCALAMIENTO',
+					'COURSE TITLE': 'AREA',
+				},
+			},
+		}
+
+		// // recorre los archivos cargados y valida si son bases correctas de la web training
+		console.log(filesList)
 		for (let i = 0; i < filesList.length; i++) {
 			const file = filesList[i]
 			const fileName = file.name.split('.')[0]
 			if (templatesDDBB.includes(fileName)) {
-				readXlsxFile(file, { map: maps[fileName] }, { trim: true })
-					.then(({ rows }) => {
-						rows.forEach((db, id) => {
-							WTLocalbase.collection(fileName).delete()
-							WTLocalbase.collection(fileName).add({
-								id,
-								...db,
-							})
-						})
-					})
-					.then(() => {
-						Swal.fire({
-							icon: 'success',
-							title: 'Base de datos actualizada',
-							text: 'Por favor presiona F5 o actualiza la pagina para cargar la base.',
-						})
-					})
-					.catch(error => {
-						Swal.fire({
-							icon: 'error',
-							title: 'Estas cargando una base de datos incorrecta',
-							text: 'Verifica que los datos en el excel esten correctamente insertados y las columnas tengan los nombres correspondientes.',
-							footer: 'Error: ' + error,
-						})
-					})
+				console.log(file)
+				readXlsxFile(file, { map }).then(({ rows }) => {
+					console.log(rows)
+					// rows.forEach(db => {
+					// 	console.log('holiss')
+					// 	WTLocalbase.collection(fileName).delete()
+					// 	WTLocalbase.collection(fileName).add(db)
+					// })
+				})
+				// .then(() => {
+				// 	Swal.fire({
+				// 		icon: 'success',
+				// 		title: 'Base de datos actualizada',
+				// 		text: 'Por favor presiona F5 o actualiza la pagina para cargar la base.',
+				// 	})
+				// })
+				// .catch(error => {
+				// 	Swal.fire({
+				// 		icon: 'error',
+				// 		title: 'Estas cargando una base de datos incorrecta',
+				// 		text: 'Verifica que los datos en el excel esten correctamente insertados y las columnas tengan los nombres correspondientes.',
+				// 		footer: 'Error: ' + error,
+				// 	})
+				// })
 			} else {
 				Swal.fire({
 					icon: 'error',
