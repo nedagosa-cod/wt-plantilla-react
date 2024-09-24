23476d3e6c5d9d1cc45f79ba25675f63cd6e91c2
nedagosa-cod <nestorcode4004@gmail.com>
2024-09-24 14:33:35 -0500

    new background

--- src/components/Bienvenida/Bienvenida.jsx
+++ src/components/Bienvenida/Bienvenida.jsx
@@ -1,12 +1,32 @@
 import { useContext } from 'react'
 import imgBack from '../../assets/images/index/backWelcome.jpg'
 import imgBackD from '../../assets/images/index/backWelcome-dark.jpg'
+import imgBgLightHalloween from '../../assets/images/index/bg-light-Halloween.jpg'
+import imgBgDarkHalloween from '../../assets/images/index/bg-dark-Halloween.jpg'
+import imgBgLightNovember from '../../assets/images/index/bg-light-november.jpg'
+import imgBgDarkNovember from '../../assets/images/index/bg-dark-november.jpg'
+import imgBgLightNavidad from '../../assets/images/index/bg-light-navidad.jpg'
+import imgBgDarkNavidad from '../../assets/images/index/bg-dark-navidad.jpg'
 import GlobalContext from '../../context/GlobalContext'
 
 export default function Bienvenida() {
 	const { scheme } = useContext(GlobalContext)
+
+	const setBackground = () => {
+		const month = new Date().getMonth() + 2
+		switch (month) {
+			case 10:
+				return `url(${scheme === 'light' ? imgBgLightHalloween : imgBgDarkHalloween})`
+			case 11:
+				return `url(${scheme === 'light' ? imgBgLightNovember : imgBgDarkNovember})`
+			case 12:
+				return `url(${scheme === 'light' ? imgBgLightNavidad : imgBgDarkNavidad})`
+			default:
+				return `url(${scheme === 'light' ? imgBack : imgBackD})`
+		}
+	}
 	const style = {
-		backgroundImage: `url(${scheme === 'light' ? imgBack : imgBackD})`,
+		backgroundImage: setBackground(),
 		colorScheme: scheme,
 	}
 	return (
@@ -16,9 +36,7 @@ export default function Bienvenida() {
 				<span>
 					Web Training <strong>Formación</strong>
 				</span>
-				<p>
-					Desarrollo de aplicativos web para la mejora de las gestiones diarias de la producción.
-				</p>
+				<p>Desarrollo de aplicativos web para la mejora de las gestiones diarias de la producción.</p>
 			</section>
 		</div>
 	)
